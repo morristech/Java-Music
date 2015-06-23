be484af20e36deb475f49c2da4f2046aa73d4f9f
nihk <nickjrose@gmail.com>
2015-06-23 13:57:56 -0400

    Edited comments

--- ChordGenerator/MyMusic.java
+++ ChordGenerator/MyMusic.java
@@ -37,13 +37,21 @@ public class MyMusic {
 		/*
 
 		c  d  e  f  g  a  b
-		g  a  b  c  d  e  f#	                  [6]		arr.length - 1
-		d  e  f# g  a  b  c#	      [2]         [6]		arr.length - 1, arr.length / 3
-		a  b  c# d  e  f# g#	      [2]      [5][6]		arr.length - 1, arr.length - 2, arr.length / 3
-		e  f# g# a  b  c# d#	   [1][2]      [5][6]		arr.length - 1, arr.length - 2, arr.length / 3, arr.length / 3 - 1
-		b  c# d# e  f# g# a#	   [1][2]   [4][5][6]		arr.length - 1, arr.length - 2, arr.length - 3, arr.length / 3, arr.length / 3 - 1
-		f# g# a# b  c# d# e#	[0][1][2]   [4][5][6]		arr.length - 1, arr.length - 2, arr.length - 3, arr.length / 3, arr.length / 3 - 1, arr.length / 3 - 2
-		c# d# e# f# g# a# b#	[0][1][2][3][4][5][6]		arr.length - 1, arr.length - 2, arr.length - 3, arr.length - 4, arr.length / 3, arr.length / 3 - 1, arr.length / 3 - 2
+		g  a  b  c  d  e  f#	                  [6]
+		d  e  f# g  a  b  c#	      [2]         [6]
+		a  b  c# d  e  f# g#	      [2]      [5][6]
+		e  f# g# a  b  c# d#	   [1][2]      [5][6]
+		b  c# d# e  f# g# a#	   [1][2]   [4][5][6]
+		f# g# a# b  c# d# e#	[0][1][2]   [4][5][6]
+		c# d# e# f# g# a# b#	[0][1][2][3][4][5][6]
+
+		arr.length - 1
+		arr.length - 1, arr.length / 3
+		arr.length - 1, arr.length - 2, arr.length / 3
+		arr.length - 1, arr.length - 2, arr.length / 3, arr.length / 3 - 1
+		arr.length - 1, arr.length - 2, arr.length - 3, arr.length / 3, arr.length / 3 - 1
+		arr.length - 1, arr.length - 2, arr.length - 3, arr.length / 3, arr.length / 3 - 1, arr.length / 3 - 2
+		arr.length - 1, arr.length - 2, arr.length - 3, arr.length - 4, arr.length / 3, arr.length / 3 - 1, arr.length / 3 - 2
 
 		c  d  e  f  g  a  b
 		d  e  f# g  a  b  c#	      [2]         [6]
