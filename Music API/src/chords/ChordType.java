8ac27a538e5483ab5be3d7390d8cb816f08062ec
nihk <nickjrose@gmail.com>
2016-09-18 21:59:03 -0400

    Cleaned up folder

--- Music API/src/chords/ChordType.java
+++
@@ -1,8 +0,0 @@
-package chords;
-
-/**
- * Created by Nick on 2016-09-17.
- */
-public enum ChordType {
-    MAJOR, MINOR, AUGMENTED, DIMINISHED
-}
--- Music API/src/src/collections/CollectionMode.java
+++
@@ -1,9 +0,0 @@
-package collections;
-
-/**
- * Created by Nick on 2016-09-17.
- * Named "CollectionMode" rather than simply "Mode" to distinguish from ChordType.java, which also has Major and Minor
- */
-public enum CollectionMode {
-    MAJOR, MINOR
-}
